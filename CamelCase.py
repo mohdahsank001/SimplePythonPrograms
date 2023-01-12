89dc3ae14f528f35684d28c766e7c8f20b79054f
Mohammed Ahsan Kollathodi <67852641+mohdahsank001@users.noreply.github.com>
2023-01-13 08:02:38 +1100

    CamelCase_hackerrank

---
+++ CamelCase.py
@@ -0,0 +1,25 @@
+'''
+SOLUTION TO CamelCase program in Python
+By Mohammed Ahsan Kollathodi 
+Created on 1/13/2023 
+
+'''
+
+
+# input the string 
+# count the number of uppercase letters in the string 
+# the total number of words will be number of uppercase letters + 1 
+
+
+
+word = input()
+
+
+count=0
+for i in word:
+      if(i.isupper()):
+            count=count+1
+            
+# The number of words will be the number of upper characters + 1 with the initial lowecase starting word 
+
+print(count+1)
